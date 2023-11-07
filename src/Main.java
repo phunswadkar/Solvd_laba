7a6f35def77837372a5164c042016bd93c1a27b5
Pallavi Hunswadkar <131616135+phunswadkar@users.noreply.github.com>
2023-11-07 08:03:21 -0800

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,43 @@
+package university_hw1;
+
+public class Main {
+  public static void main(String[] args) {
+
+    University university = new University();
+
+    for (int i = 0; i < university.getBuildings().size(); i++) {
+      System.out.println(university.getBuildings().get(i));
+    }
+
+    Building.getNoOfBuildings();
+
+
+    for (int i = 0; i < university.getPlaygrounds().size(); i++) {
+      System.out.println(university.getPlaygrounds().get(i));
+    }
+
+    for (int i = 0; i < university.getPrograms().size(); i++) {
+      System.out.println(university.getPrograms().get(i));
+    }
+
+    for (int i = 0; i < university.getParkingLots().size(); i++) {
+      System.out.println(university.getParkingLots().get(i));
+    }
+
+    for (int i = 0; i < university.getProfessors().size(); i++) {
+      System.out.println(university.getProfessors().get(i));
+    }
+
+    for (int i = 0; i < university.getStudents().size(); i++) {
+      System.out.println(university.getStudents().get(i));
+    }
+
+    Person person = university.getProfessors().get(0);
+    person.teach();
+    person.study();
+
+    person = university.getStudents().get(0);
+    person.teach();
+    person.study();
+  }
+}
---
+++ src/Student.java
@@ -0,0 +1,38 @@
+package university_hw1;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Student extends Person {
+  public Student(final String name, final LocalDate dateOfBirth, int id) {
+    super(name, dateOfBirth, id);
+  }
+
+  @Override
+  public void teach() {
+    System.out.println("Student does not teach.");
+  }
+
+  @Override
+  public void study() {
+    System.out.println("Student studies.");
+  }
+
+  public void whoami() {
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    String formattedDate = formatter.format(this.dateOfBirth);
+    System.out.println(String.format("Student " + this.name + " whose id is " + this.id + " was born on " + formattedDate ));
+  }
+
+  @Override
+  public String toString() {
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    String formattedDate = formatter.format(this.dateOfBirth);
+    return String.format("Student " + this.name + " whose id is " + this.id + " was born on " + formattedDate );
+  }
+
+  @Override
+  public int hashCode() {
+    return 800;
+  }
+}
